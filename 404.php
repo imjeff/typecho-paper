c4aa716bb25282e280e6eba455640f6a76b72d09
Jeff <imjeff@qq.com>
2023-12-16 16:21:16 +0800

    修正版本

--- 404.php
+++ 404.php
@@ -1,6 +1,7 @@
-<?php get_header(); ?>
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php $this->need('header.php'); ?>
 <div>
 <h1>404</h1>
 <p>出错了，这里什么都没有。</p>
 </div>
-<?php get_footer(); ?>
+<?php $this->need('footer.php'); ?>
